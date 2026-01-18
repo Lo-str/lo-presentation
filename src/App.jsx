db23c627130a4af2d7442611729965a6822a8f98
LoS <lostreit@pm.me>
2026-01-18 22:37:24 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home/Home.jsx"
-import Gouffre from "./pages/Gouffre/Gouffre.jsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Gouffre from "./pages/Gouffre/Gouffre.jsx";
 
 function App() {
-
-
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Gouffre" element={<Gouffre />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter basename="/lo-presentation">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Gouffre" element={<Gouffre />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
